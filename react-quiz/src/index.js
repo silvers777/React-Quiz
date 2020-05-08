993fb6d7d322563ace90c5a148e7bd690b058eb6
dmitry-iws <dmitry1991.my@mail.ru>
2020-05-08 18:46:20 +0300

    corrected layout auth

--- react-quiz/src/index.js
+++ react-quiz/src/index.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './store/reducers/rootReducer';
-import reduxThank from 'redux-thunk';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import reduxThank from 'redux-thunk'
 
 const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-    }) : compose;
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose
 
 const store = createStore(
-  rootReducer, 
-  composeEnhancers(
-    applyMiddleware(reduxThank)
-  )
+  rootReducer,
+  composeEnhancers(applyMiddleware(reduxThank))
 )
 
 const app = (
@@ -30,9 +27,9 @@ const app = (
   </Provider>
 )
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
